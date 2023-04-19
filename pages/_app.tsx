c0ec202c702d7ff3bba4e02c766ad4ee0a621056
Juan David <juandavid918@gmail.com>
2023-04-19 10:09:56 -0500

    Soporte de TypeScript agregado

---
+++ pages/_app.tsx
@@ -0,0 +1,8 @@
+import '@/styles/globals.scss';
+
+import { AppProps } from 'next/app';
+import { ReactElement } from 'react';
+
+export default function App({ Component, pageProps }: AppProps): ReactElement {
+  return <Component {...pageProps} />;
+}
