d46702df1c6e3e39bb69eb6533f295393cb40ae9
Juan David <juandavid918@gmail.com>
2023-04-20 12:03:19 -0500

    Styled Components agregados

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,28 @@
-import '@/styles/globals.scss';
-
-import type { AppProps } from 'next/app';
 import type { ReactElement } from 'react';
-
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
 import { name, description } from "package.json";
+
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+      margin: 0;
+      padding: 0;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: "#008080"
+  }
+};
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return(
@@ -15,7 +32,11 @@ export default function App({ Component, pageProps }: AppProps): ReactElement {
         <meta name="description" content={ description } />
       </Head>
 
-      <Component {...pageProps}/>
+      <GlobalStyle />
+
+      <ThemeProvider theme={ theme }>
+        <Component {...pageProps}/>
+      </ThemeProvider>
     </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,12 +2,42 @@ import type { ReactElement } from 'react';
 import type { DocumentContext, DocumentInitialProps } from 'next/document';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
+import { ServerStyleSheet } from 'styled-components';
+
 class MyDocument extends Document {
 
-  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
-    const initialProps = await Document.getInitialProps(ctx);
+  // static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+  //   const initialProps = await Document.getInitialProps(ctx);
 
-    return {...initialProps};
+  //   return {...initialProps};
+  // }
+
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const originalRenderPage = ctx.renderPage;
+    const sheet = new ServerStyleSheet();
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />)
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            { initialProps.styles }
+            { sheet.getStyleElement() }
+          </>
+        )
+      }
+    }
+    finally {
+      sheet.seal();
+    }
   }
 
   render(): ReactElement {
