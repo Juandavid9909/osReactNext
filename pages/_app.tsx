ba4131d84cb6dcabbc990bc6d8cca179c3daa09e
Juan David <juandavid918@gmail.com>
2023-04-20 11:09:28 -0500

    Etiqueta meta agregada para mejorar el SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,14 @@ import type { ReactElement } from 'react';
 
 import Head from 'next/head';
 
-import { name } from "package.json";
+import { name, description } from "package.json";
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return(
     <>
       <Head>
         <title>{ name }</title>
+        <meta name="description" content={ description } />
       </Head>
 
       <Component {...pageProps}/>
