2c475fefc45268380bc7418ba202ef068751d825
Juan David <juandavid918@gmail.com>
2023-04-27 09:39:55 -0500

    Implementado el process loader

---
+++ contexts/process.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+import processDirectory from "utils/processDirectory";
+
+const ProcessLoader: FC = () => (
+  <>
+    {  Object.entries(processDirectory).map(([id, { Component }]) => <Component key={ id } />) }
+   </>
+);
+
+export default ProcessLoader;
