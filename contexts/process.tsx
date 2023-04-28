a442b3c643ccdcef79e76b869fb3f3619bb23fcf
Juan David <juandavid918@gmail.com>
2023-04-28 11:49:09 -0500

    Creados los states de Initial Context

--- contexts/process.tsx
+++ contexts/process.tsx
@@ -1,12 +1,12 @@
 import useProcessContextState from "hooks/useProcessContextState";
 import type { FC } from "react";
 import { createContext } from "react";
-import type { ProcessContextState } from "types/contexts/process";
 import type { ProcessProps } from "types/components/pages/Process";
+import type { ProcessContextState } from "types/contexts/process";
+import { initialProcessContextState } from "utils/initialContextStates";
 import processDirectory from "utils/processDirectory";
 
-
-const ProcessContext = createContext<ProcessContextState>({ processes: {} });
+const ProcessContext = createContext<ProcessContextState>(initialProcessContextState);
 
 export const ProcessProvider: FC<ProcessProps> = ({ children }) => (
   <ProcessContext.Provider value={ useProcessContextState(processDirectory) }>
--- contexts/session.tsx
+++ contexts/session.tsx
@@ -3,8 +3,9 @@ import type { FC } from "react";
 import { createContext } from "react";
 import type { ProcessProps } from "types/components/pages/Process";
 import type { SessionContextState } from "types/contexts/session";
+import { initialSessionContextState } from "utils/initialContextStates";
 
-const SessionContext = createContext<SessionContextState>({});
+const SessionContext = createContext<SessionContextState>(initialSessionContextState);
 
 export const SessionProvider: FC<ProcessProps> = ({ children }) => (
   <SessionContext.Provider value={ useSessionContextState() }>
