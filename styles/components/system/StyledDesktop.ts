668f40495855c27a337d45e08f6c2730a53ea35d
Juan David <juandavid918@gmail.com>
2023-04-28 14:30:32 -0500

    Usado nuevo color de tema para el background

--- styles/components/system/StyledDesktop.ts
+++ styles/components/system/StyledDesktop.ts
@@ -8,7 +8,7 @@ const StyledDesktop = styled.main`
   bottom: 0;
   left: 0;
   right: 0;
-  background-color: #000;
+  background-color: ${ ({ theme }) => theme.colors.backgroundcolor };
 `;
 
 export default StyledDesktop;
