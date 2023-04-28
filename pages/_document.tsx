e99ce50a636af89c5e16c3405efec374a807cd79
Juan David <juandavid918@gmail.com>
2023-04-28 13:12:09 -0500

    Configuración HTML lang en archivo next.config.js

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,24 +1,11 @@
 import type { DocumentContext, DocumentInitialProps } from 'next/document';
-import Document, { Head, Html, Main, NextScript } from 'next/document';
-import type { ReactElement } from 'react';
+import Document from 'next/document';
 import withServerStyleSheet from 'utils/withServerStyleSheet';
 
 class MyDocument extends Document {
 
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     return withServerStyleSheet(ctx);
-  }
-
-  render(): ReactElement {
-    return (
-      <Html lang="en">
-        <Head/>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
   }
 }
 
