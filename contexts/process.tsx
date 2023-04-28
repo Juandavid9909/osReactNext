3c3bed6ae834a94bc803ecc6c0309b72225c23dc
Juan David <juandavid918@gmail.com>
2023-04-28 07:21:32 -0500

    Refactorizado el processStateContext

--- contexts/process.tsx
+++ contexts/process.tsx
@@ -1,22 +1,19 @@
 import type { FC } from "react";
-import { createContext, useState } from "react";
+import { createContext } from "react";
 
 import type { ProcessContextState } from "types/contexts/process";
 import type { ProcessProps } from "types/components/pages/Process";
 
 import processDirectory from "utils/processDirectory";
 
+import useProcessContextState from "hooks/useProcessContextState";
+
 const ProcessContext = createContext<ProcessContextState>({ processes: {} });
 
-export const ProcessProvider: FC<ProcessProps> = ({ children }) => {
-  const [processes] = useState(processDirectory);
-
-  return (
-    <ProcessContext.Provider value={{ processes }}>
-      {/* {  Object.entries(processes).map(([id, { Component }]) => <Component key={ id } />) } */}
-      { children }
-    </ProcessContext.Provider>
-  );
-};
+export const ProcessProvider: FC<ProcessProps> = ({ children }) => (
+  <ProcessContext.Provider value={ useProcessContextState(processDirectory) }>
+    { children }
+  </ProcessContext.Provider>
+);
 
 export const ProcessConsumer = ProcessContext.Consumer;
