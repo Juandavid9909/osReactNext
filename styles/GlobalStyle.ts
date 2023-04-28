8760cf7dca659440f1e767453256db40e4a99454
Juan David <juandavid918@gmail.com>
2023-04-28 14:41:29 -0500

    Ajustados los estilos

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -1,8 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
-  *
-  *::before
+  *,
+  *::before,
   *::after {
     border: 0;
     box-sizing: border-box;
