3624b1b19bc5fc835abebb216af029f6875c7cbb
Juan David <juandavid918@gmail.com>
2023-04-29 16:04:42 -0500

    Extraer valores relevantes de Theme

--- styles/components/system/StyledDesktop.ts
+++ styles/components/system/StyledDesktop.ts
@@ -1,14 +1,14 @@
 import styled from "styled-components";
 
 const StyledDesktop = styled.main`
-  height: 100vh;
-  width: 100vw;
-  position: fixed;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
   background-color: ${ ({ theme }) => theme.colors.background };
+  bottom: 0;
+  height: 100vh;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 0;
+  width: 100vw;
 `;
 
 export default StyledDesktop;
