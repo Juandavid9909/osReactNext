20ab80927bc5cbff16f48185f6cf83dca28644d5
Juan David <juandavid918@gmail.com>
2023-04-28 19:36:00 -0500

    Construído el mockup de la barra de tareas

---
+++ styles/components/system/StyledClock.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const StyledClock = styled.button`
+  background-color: green;
+  height: 100%;
+  position: absolute;
+  right: 0;
+  width: 90px;
+`;
+
+export default StyledClock;
