78499f695669a8020214fd190cfe3d68d395760a
Juan David <juandavid918@gmail.com>
2023-04-29 16:13:06 -0500

    Centrado el contenido del componente Taskbar

--- styles/components/system/Taskbar/StyledClock.ts
+++ styles/components/system/Taskbar/StyledClock.ts
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
-const StyledClock = styled.button`
+const StyledClock = styled.time`
   background-color: green;
+  display: flex;
   height: 100%;
+  place-content: center;
+  place-items: center;
   position: absolute;
   right: 0;
   width: ${ ({ theme }) => theme.sizes.clock.width };
