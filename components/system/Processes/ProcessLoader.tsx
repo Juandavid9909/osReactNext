e86e39cf2630ed2c8b3194ff6f7e2c3d110edc65
Juan David <juandavid918@gmail.com>
2023-04-29 17:15:59 -0500

    Ajustdado ProcessesReducer a renderProcesses

--- components/system/Processes/ProcessLoader.tsx
+++ components/system/Processes/ProcessLoader.tsx
@@ -1,16 +1,14 @@
 import RenderProcess from 'components/system/Processes/RenderProcess';
 import { ProcessConsumer } from 'contexts/process';
-import { Process } from 'types/contexts/process';
+import type { Processes } from 'types/contexts/process';
 
-const ProcessesReducer = ([id, process]: [string, Process]) => (
+const renderProcesses = (processes: Processes) => Object.entries(processes).map(([id, process]) => (
   <RenderProcess key={ id } {...process} />
-);
+));
 
 const ProcessLoader = (): JSX.Element => (
   <ProcessConsumer>
-    { ({ processes }) =>
-      Object.entries(processes).map(ProcessesReducer)
-    }
+    { ({ processes }) => renderProcesses(processes) }
   </ProcessConsumer>
 )
 
