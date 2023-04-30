7218c0f25b55a253a59d2782c0b925402cb17eb7
Juan David <juandavid918@gmail.com>
2023-04-29 22:08:22 -0500

    File System implementado con BrowserFS

---
+++ hooks/useFileSystemContextState.ts
@@ -0,0 +1,24 @@
+// const BrowserFS = require("node_modules/browserfs/src/core/browserfs");
+import * as BrowserFS from "browserfs";
+import type { FSModule } from "browserfs/dist/node/core/FS";
+import { useEffect, useState } from "react";
+import type { FileSystemContextState } from "types/contexts/fileSystem";
+
+const useFileSystemContextState = (): FileSystemContextState => {
+  const [fs, setFs] = useState<FSModule | null>(null);
+
+  useEffect(() => {
+    BrowserFS.install(window);
+
+    BrowserFS.configure({
+      fs: "IndexedDb"
+    }, () => {
+      setFs(BrowserFS.BFSRequire("fs"));
+    });
+  }, [setFs]);
+
+  {/* @ts-ignore */}
+  return { fs };
+}
+
+export default useFileSystemContextState;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,22 @@
 import Metadata from 'components/pages/Metadata';
 import StyledApp from 'components/pages/StyledApp';
+import { FileSystemProvider } from 'contexts/fileSystem';
 import { SessionProvider } from 'contexts/session';
 import type { AppProps } from 'next/app';
 
 const  App = ({ Component, pageProps }: AppProps): React.ReactElement => (
   <>
     {/* @ts-ignore */}
-    <SessionProvider>
-      <StyledApp>
-        <Metadata />
+    <FileSystemProvider>
+      {/* @ts-ignore */}
+      <SessionProvider>
+        <StyledApp>
+          <Metadata />
 
-        <Component {...pageProps}/>
-      </StyledApp>
-    </SessionProvider>
+          <Component {...pageProps}/>
+        </StyledApp>
+      </SessionProvider>
+    </FileSystemProvider>
   </>
 );
 
