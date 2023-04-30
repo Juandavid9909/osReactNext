c41af5fee204a907a6fee7091b36f6077ef82846
Juan David <juandavid918@gmail.com>
2023-04-30 10:32:05 -0500

    Agregadas taskbarEntries e icon/title al process

--- components/system/Taskbar/TaskbarEntries.tsx
+++ components/system/Taskbar/TaskbarEntries.tsx
@@ -1,9 +1,14 @@
 import TaskbarEntry from "components/system/Taskbar/TaskbarEntry";
+import { ProcessConsumer } from "contexts/process";
 import StyledTaskbarEntries from "styles/components/system/Taskbar/StyledTaskbarEntries";
 
 const TaskbarEntries = (): JSX.Element => (
   <StyledTaskbarEntries>
-    <TaskbarEntry />
+    <ProcessConsumer>
+      { ({ processes }) => Object.entries(processes).map(([id, { icon, title }]) => (
+        <TaskbarEntry key={ id } icon={ icon } title={ title } />
+      )) }
+    </ProcessConsumer>
   </StyledTaskbarEntries>
 );
 
