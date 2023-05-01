dc9dd8f0dddcbf204e8fb6eb751924676a1668ce
Juan David <juandavid918@gmail.com>
2023-05-01 13:12:00 -0500

    useFileInfo hook y obtener shortcuts

---
+++ utils/fileFunctions.ts
@@ -0,0 +1,17 @@
+import type { FSModule } from "browserfs/src/core/FS";
+import ini from "ini";
+
+type Shortcut = {
+  URL: string,
+  IconFile: string
+};
+
+export const getShortcut = (path: string, fs: FSModule): Promise<Shortcut> => {
+  return new Promise((resolve) => {
+    fs.readFile(path, (_error, contents = Buffer.from("")) =>
+      resolve(ini.parse(contents.toString()) as Shortcut)
+    );
+  });
+}
+
+export const getProcessByFileExtension = (_extension: string): string => "";
