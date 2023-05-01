3d1be2c2d4b8c87a769b278289fc11480bc16fca
Juan David <juandavid918@gmail.com>
2023-05-01 13:58:25 -0500

    Creado el FileManager y useFiles hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Desktop from 'components/system/Desktop';
+import FileManager from 'components/system/Files/FileManager';
 import Taskbar from 'components/system/Taskbar';
 import ProcessLoader from 'components/system/Processes/ProcessLoader';
 import { ProcessProvider } from 'contexts/process';
@@ -7,6 +8,8 @@ const Home = (): React.ReactElement => (
   <Desktop>
     {/* @ts-ignore */}
     <ProcessProvider>
+        <FileManager directory="/desktop" />
+
         <Taskbar />
 
         <ProcessLoader />
--- utils/fileFunctions.ts
+++ utils/fileFunctions.ts
@@ -8,9 +8,11 @@ type Shortcut = {
 
 export const getShortcut = (path: string, fs: FSModule): Promise<Shortcut> => {
   return new Promise((resolve) => {
-    fs.readFile(path, (_error, contents = Buffer.from("")) =>
-      resolve(ini.parse(contents.toString()) as Shortcut)
-    );
+    fs.readFile(path, (_error, contents = Buffer.from("")) => {
+      const { InternetShortcut = { URL: "", IconFile: "" } } = ini.parse(contents.toString());
+
+      resolve(InternetShortcut as Shortcut);
+    });
   });
 }
 
