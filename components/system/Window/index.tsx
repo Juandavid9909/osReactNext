06f7e56ccf2b9baea3fbcb45fd5df1d79638bac4
Juan David <juandavid918@gmail.com>
2023-05-02 12:02:58 -0500

    Guardando el tamaño y la posición de la ventana en state

--- components/system/Window/index.tsx
+++ components/system/Window/index.tsx
@@ -1,8 +1,8 @@
 import type { ProcessComponentProps } from "components/system/Processes/RenderProcess";
+import RndWindow from "components/system/Window/RndWindow";
 import Titlebar from "components/system/Window/Titlebar";
 import { useProcesses } from "contexts/process";
-import useRnd from "hooks/useRnd";
-import { Rnd } from "react-rnd";
+import React from "react";
 import StyledWindow from "styles/components/system/Window/StyledWindow";
 
 type WindowProps = ProcessComponentProps & {
@@ -10,18 +10,16 @@ type WindowProps = ProcessComponentProps & {
 };
 
 const Window = ({ children, id }: WindowProps): JSX.Element => {
-  const { processes: { [id]: { maximized, minimized } } } = useProcesses();
-
-  const rndProps = useRnd(maximized);
+  const { processes: { [id]: { minimized } } } = useProcesses();
 
   return (
-    <Rnd {...rndProps}>
+    <RndWindow id={ id }>
       <StyledWindow minimized={ minimized }>
         <Titlebar id={ id } />
 
         { children }
       </StyledWindow>
-    </Rnd>
+    </RndWindow>
   );
 }
 
