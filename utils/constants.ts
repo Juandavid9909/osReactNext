aa65f32a8864d1068cc71fe81b0f5a905bcb2fdd
Juan David <juandavid918@gmail.com>
2023-05-02 08:38:18 -0500

    Extraídas constantes de Window

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,3 +1,16 @@
-export const MILLISECONDS_IN_SECOND = 1000;
+import type { Size } from "hooks/useDraggableAndResizable";
+import type { Position } from "react-rnd";
+
+export const DEFAULT_WINDOW_POSITION: Position = {
+  x: 0,
+  y: 0
+}
+
+export const DEFAULT_WINDOW_SIZE: Size = {
+  height: "200px",
+  width: "250px"
+}
 
 export const IMAGE_FILE_EXTENSIONS = [".apng", ".avif", ".bmp", ".cur", ".gif", ".ico", ".jfif", ".jif", ".jpe", ".jpeg", ".jpg", ".pjp", ".pjpeg", ".png", ".svg", ".tif", ".tiff", ".webp", ".xbm"];
+
+export const MILLISECONDS_IN_SECOND = 1000;
