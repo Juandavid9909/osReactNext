37de58292ff3f803c7ff82be665f2953846b3219
Juan David <juandavid918@gmail.com>
2023-05-02 12:52:59 -0500

    Refactorizado archivos en state y agregado fallback

--- utils/fileFunctions.ts
+++ utils/fileFunctions.ts
@@ -1,4 +1,4 @@
-import type { FSModule } from "browserfs/src/core/FS";
+import type { FSModule } from "browserfs/dist/node/core/FS";
 import ini from "ini";
 
 type Shortcut = {
@@ -7,13 +7,25 @@ type Shortcut = {
 };
 
 export const getShortcut = (path: string, fs: FSModule): Promise<Shortcut> => {
-  return new Promise((resolve) => {
-    fs.readFile(path, (_error, contents = Buffer.from("")) => {
-      const { InternetShortcut = { URL: "", IconFile: "" } } = ini.parse(contents.toString());
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, (error, contents = Buffer.from("")) => {
+      if(error) {
+        reject(error);
+      }
+      else {
+        const { InternetShortcut = { URL: "", IconFile: "" } } = ini.parse(contents.toString());
 
-      resolve(InternetShortcut as Shortcut);
+        resolve(InternetShortcut as Shortcut);
+      }
     });
   });
+}
+
+export const getIconByFileExtension = (extension: string): string => {
+  switch(extension) {
+    default:
+      return "/icons/unknown.ico";
+  }
 }
 
 export const getProcessByFileExtension = (_extension: string): string => "";
