fa469d9947c3cb09d6fcb867f1cd9228de7e622b
Juan David <juandavid918@gmail.com>
2023-05-11 11:50:35 -0500

    setOptions para el color de vantaWaves

--- components/system/Desktop/wallpapers/vantaWaves.ts
+++ components/system/Desktop/wallpapers/vantaWaves.ts
@@ -27,9 +27,7 @@ const vantaWaves = (settings: VantaWavesSettings): WallpaperEffect => (element) 
   }) : undefined;
 
   if(vantaEffect) {
-    const { onDestroy } = colorCycle(settings.color, (color) => {
-      vantaEffect.options.color = color;
-    });
+    const { onDestroy } = colorCycle(settings.color, (color) => vantaEffect.setOptions({ color }));
 
     vantaEffect.onDestroy = onDestroy;
   }
