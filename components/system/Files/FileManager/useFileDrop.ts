b1aca0f4cc408442acb5a78eaabaff68bd42cf64
Juan David <juandavid918@gmail.com>
2023-05-11 13:10:46 -0500

    Sólo carga archivos correctos

--- components/system/Files/FileManager/useFileDrop.ts
+++ components/system/Files/FileManager/useFileDrop.ts
@@ -17,17 +17,20 @@ const useFileDrop = (directory: string, updateFiles: (appendFile?: string) => vo
     haltDragEvent(event);
 
     const { files: [file] = [] } = event.dataTransfer || {};
-    const reader = new FileReader();
 
-    reader.onload = ({ target }) => {
-      fs?.writeFile(
-        `${ directory }/${ file.name }`,
-        Buffer.from(new Uint8Array(target?.result as ArrayBuffer)),
-        (error) => !error && updateFiles(file.name)
-      );
+    if(file) {
+      const reader = new FileReader();
+
+      reader.onload = ({ target }) => {
+        fs?.writeFile(
+          `${ directory }/${ file.name }`,
+          Buffer.from(new Uint8Array(target?.result as ArrayBuffer)),
+          (error) => !error && updateFiles(file.name)
+        );
+      }
+
+      reader.readAsArrayBuffer(file);
     }
-
-    reader.readAsArrayBuffer(file);
   }, [directory, fs, updateFiles]);
 
   return {
