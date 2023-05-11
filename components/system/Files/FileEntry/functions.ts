67038aa84e5e5602e53a94fd1a360e63f9006b83
Juan David <juandavid918@gmail.com>
2023-05-11 16:43:03 -0500

    Agregado JSDOS y DOOM

--- components/system/Files/FileEntry/functions.ts
+++ components/system/Files/FileEntry/functions.ts
@@ -1,25 +1,23 @@
 export const getIconByFileExtension = (extension: string): string => {
-  switch(extension) {
-    case ".img":
-      return "";
-
-    case ".iso":
-      return "/icons/image.png";
-
+  switch (extension) {
+    case '.img':
+    case '.iso':
+      return '/icons/image.png';
+    case '.jsdos':
+      return '/icons/compressed.png';
     default:
-      return "/icons/unknown.png";
+      return '/icons/unknown.png';
   }
-}
+};
 
 export const getProcessByFileExtension = (extension: string): string => {
-  switch(extension) {
-    case ".img":
-      return "";
-
-    case ".iso":
-      return "V86";
-
+  switch (extension) {
+    case '.img':
+    case '.iso':
+      return 'V86';
+    case '.jsdos':
+      return 'JSDOS';
     default:
-      return "";
+      return '';
   }
 };
