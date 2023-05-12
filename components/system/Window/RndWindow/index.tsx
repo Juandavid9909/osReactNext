fe5a385b3a9151103e1ebf79079f220a8039799b
Juan David <juandavid918@gmail.com>
2023-05-12 09:51:54 -0500

    Arreglado el focus cuando redimensionamos ventanas

--- components/system/Window/RndWindow/index.tsx
+++ components/system/Window/RndWindow/index.tsx
@@ -1,9 +1,10 @@
-import useRnd from "components/system/Window/RndWindow/useRnd";
-import { useProcesses } from "contexts/process";
-import { useSession } from "contexts/session";
-import { CSSProperties, useEffect, useRef } from "react";
-import { Rnd } from "react-rnd";
-import { DEFAULT_WINDOW_SIZE } from "utils/constants";
+import useRnd from 'components/system/Window/RndWindow/useRnd';
+import { useProcesses } from 'contexts/process';
+import { useSession } from 'contexts/session';
+import type { CSSProperties } from 'react';
+import { useEffect, useRef } from 'react';
+import { Rnd } from 'react-rnd';
+import { DEFAULT_WINDOW_SIZE } from 'utils/constants';
 
 type RndWindowProps = {
   children: React.ReactNode,
@@ -11,27 +12,46 @@ type RndWindowProps = {
   style: CSSProperties
 };
 
-const RndWindow = ({ children, id, style }: RndWindowProps): JSX.Element => {
-  const { processes: { [id]: { autoSizing, maximized } } } = useProcesses();
+const reRouteFocus = (focusElement?: HTMLElement) => (
+  element?: Element
+): void => {
+  element?.setAttribute('tabindex', '-1');
+  element?.addEventListener('mousedown', (event) => {
+    event.preventDefault();
+    focusElement?.focus();
+  });
+};
 
+const RndWindow = ({ children, id, style }: RndWindowProps): JSX.Element => {
+  const {
+    processes: {
+      [id]: { autoSizing, maximized }
+    }
+  } = useProcesses();
   const rndRef = useRef<Rnd | null>(null);
   const rndProps = useRnd(id, maximized);
   const { setWindowStates } = useSession();
 
   useEffect(() => {
     const { current } = rndRef || {};
+    const [windowContainer, resizeHandleContainer] =
+      current?.resizableElement?.current?.children || [];
+    const resizeHandles = [...resizeHandleContainer?.children];
 
-    return () =>  setWindowStates((currentWindowStates) => ({
-      ...currentWindowStates,
-      [id]: {
-        position: current?.props?.position,
-        size: autoSizing ?  DEFAULT_WINDOW_SIZE : current?.props?.size
-      }
-    }))
+    resizeHandles.forEach(reRouteFocus(windowContainer as HTMLElement));
+
+    return () =>
+      setWindowStates((currentWindowStates) => ({
+        ...currentWindowStates,
+        [id]: {
+          position: current?.props?.position,
+          size: autoSizing ? DEFAULT_WINDOW_SIZE : current?.props?.size
+        }
+      }));
   }, [autoSizing, id, setWindowStates]);
 
   return (
-    <Rnd ref={ rndRef } style={ style } {...rndProps}>
+    <Rnd ref={ rndRef } style={ style } { ...rndProps }>
       { children }
     </Rnd>
   );
