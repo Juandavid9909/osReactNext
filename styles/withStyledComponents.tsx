b9455dc0b477786f670ff827442385754ef56a22
Juan David <juandavid918@gmail.com>
2023-05-12 09:44:09 -0500

    Ajustes ESLint

--- styles/withStyledComponents.tsx
+++ styles/withStyledComponents.tsx
@@ -2,27 +2,27 @@ import type { DocumentContext, DocumentInitialProps } from 'next/document';
 import Document from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
-const withStyledComponents = async (ctx: DocumentContext): Promise<DocumentInitialProps> => {
-  const originalRenderPage = ctx.renderPage;
-    const sheet = new ServerStyleSheet();
+const withStyledComponents = async (
+  ctx: DocumentContext
+): Promise<DocumentInitialProps> => {
+  const { renderPage } = ctx;
+  const sheet = new ServerStyleSheet();
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />)
-        });
+  try {
+    ctx.renderPage = () =>
+      renderPage({
+        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
+      });
 
-      const { styles, ...initialProps } = await Document.getInitialProps(ctx);
+    const { styles, ...initialProps } = await Document.getInitialProps(ctx);
 
-      return {
-        ...initialProps,
-        styles: [styles, sheet.getStyleElement()]
-      }
-    }
-    finally {
-      sheet.seal();
-    }
+    return {
+      ...initialProps,
+      styles: [styles, sheet.getStyleElement()]
+    };
+  } finally {
+    sheet.seal();
+  }
 }
 
 export default withStyledComponents;
