edf8be626d3725e81987666b2c07b2ca11536651
Juan David <juandavid918@gmail.com>
2023-05-12 09:36:22 -0500

    Iterar nombres de archivos

--- components/system/Files/FileManager/useFileDrop.ts
+++ components/system/Files/FileManager/useFileDrop.ts
@@ -1,37 +1,71 @@
-import { useFileSystem } from "contexts/fileSystem";
-import { useCallback } from "react";
+import { useFileSystem } from 'contexts/fileSystem';
+import { basename, dirname, extname } from 'path';
+import { useCallback } from 'react';
 
 const haltDragEvent = (event: React.DragEvent<HTMLElement>): void => {
   event.preventDefault();
   event.stopPropagation();
-}
+};
 
 type FileDrop = {
-  onDragOver: (event:React.DragEvent<HTMLElement>) => void,
+  onDragOver: (event: React.DragEvent<HTMLElement>) => void,
   onDrop: (event: React.DragEvent<HTMLElement>) => void
 };
 
-const useFileDrop = (directory: string, updateFiles: (appendFile?: string) => void): FileDrop => {
+const iterateFileName = (path: string, iteration: number): string => {
+  const extension = extname(path);
+  const fileName = basename(path, extension);
+
+  return `${ dirname(path) }/${ fileName } (${ iteration })${ extension }`;
+};
+
+const useFileDrop = (
+  directory: string,
+  updateFiles: (appendFile?: string) => void
+): FileDrop => {
   const { fs } = useFileSystem();
-  const onDrop = useCallback((event: React.DragEvent<HTMLElement>) => {
-    haltDragEvent(event);
+  const onDrop = useCallback(
+    (event: React.DragEvent<HTMLElement>) => {
+      haltDragEvent(event);
 
-    const { files: [file] = [] } = event.dataTransfer || {};
+      const { files: [file] = [] } = event.dataTransfer || {};
 
-    if(file) {
-      const reader = new FileReader();
+      if (file) {
+        const writeUniqueName = (
+          path: string,
+          fileBuffer: Buffer,
+          iteration = 0
+        ): void => {
+          const writePath = !iteration
+            ? path
+            : iterateFileName(path, iteration);
 
-      reader.onload = ({ target }) => {
-        fs?.writeFile(
-          `${ directory }/${ file.name }`,
-          Buffer.from(new Uint8Array(target?.result as ArrayBuffer)),
-          (error) => !error && updateFiles(file.name)
-        );
+          fs?.stat(writePath, (statError) => {
+            if (statError?.code === 'ENOENT') {
+              fs?.writeFile(
+                writePath,
+                fileBuffer,
+                (writeError) => !writeError && updateFiles(writePath)
+              );
+            } else {
+              writeUniqueName(path, fileBuffer, iteration + 1);
+            }
+          });
+        };
+        const reader = new FileReader();
+
+        reader.onload = ({ target }) => {
+          writeUniqueName(
+            `${ directory }/${ file.name }`,
+            Buffer.from(new Uint8Array(target?.result as ArrayBuffer))
+          );
+        };
+
+        reader.readAsArrayBuffer(file);
       }
-
-      reader.readAsArrayBuffer(file);
-    }
-  }, [directory, fs, updateFiles]);
+    },
+    [directory, fs, updateFiles]
+  );
 
   return {
     onDragOver: haltDragEvent,
